47904072bc9d1087bb95a8165ba4a5694895314b
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-04-02 20:54:50 -0300

    implentando a criação de projeto

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -1,14 +1,56 @@
-window.onload = function() {
-    // Type: 'create' | 'edit'
-    const screenType = 'create';
+// Type: 'create' | 'edit'
+let screenType = "create";
 
-    if(screenType == 'create'){
-        document.querySelector('#main_title').innerText = 'Vamos cadastrar seu novo projeto!';
-        document.querySelector('#df_button').innerText = 'Cadastrar';
-    }else
+// Obtém os parametros da url
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+window.onload = function () {
+  SetScreenTypeText();
+};
+
+function SetScreenTypeText() {
+  screenType = params.id ? "edit" : "create";
+
+  if (screenType == "create") {
+    document.querySelector("#main_title").innerText =
+      "Vamos cadastrar seu novo projeto!";
+    document.querySelector("#df_button").innerText = "Cadastrar";
+  } else {
+    document.querySelector("#main_title").innerText = "Editar projeto";
+    document.querySelector("#df_button").innerText = "Salvar";
+  }
+}
+
+function createOrEdit(event) {
+  event.preventDefault();
+
+  let payload = {
+    title: document.querySelector("#title").value,
+    description: document.querySelector("#description").value,
+    totalCost: document.querySelector("#totalCost").value,
+    idClient: "1",
+  };
+
+  // Enviar para API
+  fetch(
+    `https://660b4ff2ccda4cbc75dca830.mockapi.io/api/projects${
+      screenType === "edit" ?  `/${params.id}`: ""
+    }`,
     {
-        document.querySelector('#main_title').innerText = 'Editar projeto';
-        document.querySelector('#df_button').innerText = 'Salvar';
+      method: screenType === 'edit' ? "PUT" : "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
-
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      if (screenType === "edit") alert("Editado com sucesso");
+      else alert("Cadastrado com sucesso");
+    })
+    .catch((error) => {
+      alert("Erro no servidor");
+    });
 }
